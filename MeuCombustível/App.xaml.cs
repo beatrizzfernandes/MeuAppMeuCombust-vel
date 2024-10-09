88463e82ec84e8ad87b7e2702154df9ad53ed3c8
Beatriz Fernandes <fernandes.beatrzz@gmail.com>
2024-10-09 10:52:52 -0300

    Adicionar arquivos de projeto.

---
+++ MeuCombustível/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MeuCombustível
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MeuCombustível/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MeuCombustível
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
